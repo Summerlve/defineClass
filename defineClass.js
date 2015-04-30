91bf172d97704cdf0adf8b99a39cb300304ee3e3
张昊 <dzxx0563zh@126.com>
2015-04-30 11:56:50 +0800

    简直操了

--- defineClass.js
+++ defineClass.js
@@ -1,10 +1,6 @@
-"use strict"
+"use strict";
 
-function isinstance (o, c) {
-	var types = o._type;
-	return c in types;
-}
-
+// 寻找父类，再返回父类构造函数bind了子类this的wrapper
 function _super (klass, scope) {
 	return {
 		init: klass._super.prototype.init.bind(scope)
@@ -12,42 +8,64 @@ function _super (klass, scope) {
 }
 
 function defineClass (superClass, props) {
-	var events = props.events ? props.events : null;
-	var fn = function () {
-		var types = []
-		this.type = fn;
-		if (events !== null && typeof events["beforeInit"] === "function") events["beforeInit"]();
+	var klass = function () {
+		// type 不能删除，不能更改，可以枚举
+		Object.defineProperty(this, "type", {
+			configurable: false,
+			enumerable: true,
+			writable: false,
+			value: klass
+		});
+		
 		this.init.apply(this, arguments);
 	};
 	
+	// 判断是否有需要继承
 	if (superClass !== null) {
-		fn.prototype = new superClass();
-		fn._super = superClass;
-	} else {
-		fn._super = null;
-	}
-	
-	Object.defineProperties(fn.prototype, {
-		constructor: {
-			configurable: false,
-			enumerable: false,
-			writeable: false,
-			value: fn
-		}
-	});
-	
-	fn.prototype.init = function () {};
-		 
-	for (var key in props) {		
-		Object.defineProperty(fn.prototype, key, {
+		klass.prototype = new superClass();
+		Object.defineProperty(klass, "_super", {
 			configurable: false,
 			enumerable: true,
-			writeable: true,
+			writable: false,
+			value: superClass
+		});
+	} else {
+		Object.defineProperty(klass, "_super", {
+			configurable: false,
+			enumerable: true,
+			writable: false,
+			value: null
+		});
+	}
+	
+	Object.defineProperty(klass.prototype, "constructor", {
+		configurable: false,
+		enumerable: false,
+		writable: false,
+		value: klass
+	});
+	
+	
+	for (var key in props) {
+		// init 不可枚举
+		if (key === "init") {
+			Object.defineProperty(klass.prototype, key, {
+				configurable: false,
+				enumerable: false,
+				writeable: false,
+				value: props[key]
+			});
+			continue ;
+		}
+		Object.defineProperty(klass.prototype, key, {
+			configurable: false,
+			enumerable: true,
+			writeable: false,
 			value: props[key]
 		});
 	}
 	
-	return fn;
+	return klass;
 }
 
 
