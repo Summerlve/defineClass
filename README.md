8d8af983d6e56197fdf32cfb3b7f5dab856bb9d3
张昊 <dzxx0563zh@126.com>
2015-05-02 20:21:43 +0800

    funny

--- README.md
+++ README.md
@@ -27,20 +27,6 @@ var Student = defineClass(People, {
 
 var s = new Student("zhanghao", 22, 12261018);
 s.say();
-
-		
-var Thr = defineClass(Student, {
-	init: function (name, age, stuId, score) {
-		_super(Thr, this).init(name, age, stuId);
-		this.score = score;
-	},
-	say: function () {
-		console.log(this.name + " " + this.age + " " + this.stuId + " " + this.score);
-	}
-});
-
-var t = new Thr("zhanghao", 22, 1226, 123123);
-t.say();
 ```
 
 
--- defineClass.js
+++ defineClass.js
@@ -7,9 +7,17 @@ function _super (klass, scope) {
 	}
 }
 
+function classMethod () {}
+
 function defineClass (superClass, props) {
-	if (superClass !== null && typeof superClass !== "function") throw new TypeError("superClass's type is worng");
+	// 继承只支持单重继承，单继承
+	if (superClass !== null && superClass._super !== null) throw new Error("继承只支持单重继承，单继承");
+	// superClass为可选项，值为null或者一个类
+	if (superClass !== null && typeof superClass !== "function") throw new TypeError("superClass is opptional, if the class dont inherit any class, must choose null here");
+	// props必须是obj
+	if (!(props instanceof Object)) throw new TypeError("props must be a obj");
 	var klass = function () {
+//		if (!(this instanceof kalss)) throw new Error("dont forget the keyword 'new'");
 		// type 不能删除，不能更改，可以枚举
 		Object.defineProperty(this, "type", {
 			configurable: false,
@@ -26,14 +34,14 @@ function defineClass (superClass, props) {
 		klass.prototype = new superClass();
 		Object.defineProperty(klass, "_super", {
 			configurable: false,
-			enumerable: true,
+			enumerable: false,
 			writable: false,
 			value: superClass
 		});
 	} else {
 		Object.defineProperty(klass, "_super", {
 			configurable: false,
-			enumerable: true,
+			enumerable: false,
 			writable: false,
 			value: null
 		});
@@ -47,7 +55,7 @@ function defineClass (superClass, props) {
 		value: klass
 	});
 	
-	// 讲方法导入klass的原型
+	// 将方法导入klass的原型
 	for (var key in props) {
 		// init 不可枚举，不能删除，不能更改
 		if (key === "init") {
@@ -71,6 +79,89 @@ function defineClass (superClass, props) {
 }
 
 
+// 简单的Stack和Queue实现
+var Node = defineClass(null, {
+	init: function (item) {
+		this.item = item;
+		this.next = null;
+	}
+});
+
+var Stack = defineClass(null, {
+	init: function () {
+		this.first = null;
+		this.N = 0;
+	},
+	size: function () {
+		return this.N;
+	},
+	isEmpty: function () {
+		return this.first === null;
+	},
+	push: function (item) {
+		var oldfirst = this.first;
+		this.first = new Node(item);
+		this.first.next = oldfirst;
+		++ this.N;
+		return this;
+	},
+	pop: function () {
+		if (this.isEmpty()) throw "Stack为空";
+		var item = this.first.item;
+		this.first = this.first.next;
+		-- this.N;
+		return item;
+	},
+	foreach: function (fn) {
+		if (this.isEmpty()) throw "Stack为空";
+		var cur = this.first;
+		while (cur !== null) {
+			fn.call(null, cur.item);
+			cur = cur.next;
+		}
+	}
+});
+
+var Queue = defineClass(null, {
+	init: function () {
+		this.first = null;
+		this.last = null;
+		this.N = 0
+	},
+	size: function () {
+		return this.N;
+	},
+	isEmpty: function () {
+		return this.first === null;
+	},
+	enqueue: function (item) {
+		var oldlast = this.last;
+		this.last = new Node(item);
+		if (this.isEmpty()) {
+			this.first = this.last;
+		} else {
+			oldlast.next = this.last;
+		}
+		++ this.N;
+		return this;
+	},
+	dequeue: function (item) {
+		if (this.isEmpty()) throw new Error("Queue为空");
+		var item = this.first.item;
+		this.first = this.first.next;
+		if (this.isEmpty()) this.last = null;
+		-- this.N;
+		return item;
+	},
+	foreach: function (fn) {
+		if (this.isEmpty()) throw new Error("Queue为空"); 
+		var cur = this.first;
+		while (cur !== null) {
+			fn.call(null, cur.item);
+			cur = cur.next;
+		}
+	}
+});
 
 
 
